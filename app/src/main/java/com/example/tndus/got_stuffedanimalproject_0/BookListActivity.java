2c4d193e6fcf4a18d87d960a1845a878c9793209
tndus <tndusdl73@naver.com>
2018-09-16 18:00:39 +0900

    BookListActivity에서 리스트 목록 누르면 간단한 mp3파일 재생 -> 에뮬레이터 작동 안함..

--- app/src/main/java/com/example/tndus/got_stuffedanimalproject_0/BookListActivity.java
+++ app/src/main/java/com/example/tndus/got_stuffedanimalproject_0/BookListActivity.java
@@ -1,13 +1,58 @@
 package com.example.tndus.got_stuffedanimalproject_0;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookListActivity extends AppCompatActivity {
+
+    List<String> list;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+
+
+        list = new ArrayList<>();
+
+        Field[] fields = R.raw.class.getFields();
+        for(int i=0; i<fields.length; i++){
+            list.add(fields[i].getName());
+        }
+
+        //remove first two elements
+        list.remove(0);
+        list.remove(0);
+
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
+        ListView listView = (ListView) findViewById(R.id.bookListView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        if (mediaPlayer != null){
+                            mediaPlayer.release();
+                        }
+
+                        int reaID = getResources().getIdentifier(list.get(position),"raw",getPackageName());
+                        mediaPlayer = MediaPlayer.create(BookListActivity.this,reaID);
+                        mediaPlayer.start();
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/com/example/tndus/got_stuffedanimalproject_0/ReadBookActivity.java
+++ app/src/main/java/com/example/tndus/got_stuffedanimalproject_0/ReadBookActivity.java
@@ -1,7 +1,10 @@
 package com.example.tndus.got_stuffedanimalproject_0;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ReadBookActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class ReadBookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_book);
+
+        Button bookListButton = (Button)findViewById(R.id.bookListButton);
+        Button bookDownloadButton = (Button)findViewById(R.id.bookDownloadButton);
+
+        bookListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bookListIntent = new Intent(ReadBookActivity.this, BookListActivity.class);
+                ReadBookActivity.this.startActivity(bookListIntent);
+            }
+        });
+
+        bookDownloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bookDownloadIntent = new Intent(ReadBookActivity.this, BookDownloadActivity.class);
+                ReadBookActivity.this.startActivity(bookDownloadIntent);
+            }
+        });
+
+
     }
 }
